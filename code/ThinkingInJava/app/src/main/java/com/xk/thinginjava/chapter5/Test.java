6f40c62ffd7f430f0321f7b7523b8cc54f84954f
薛凯 <3440395@qq.com>
2017-04-27 23:55:47 +0800

    垃圾回收机看完

--- code/ThinkingInJava/app/src/main/java/com/xk/thinginjava/chapter5/Test.java
+++ code/ThinkingInJava/app/src/main/java/com/xk/thinginjava/chapter5/Test.java
@@ -5,7 +5,14 @@ package com.xk.thinginjava.chapter5;
  */
 
 public class Test {
+
     public static void main(String[] args){
         System.out.println("xx");
+    }
+    public void main(){
+        int i;
+i=1;
+        int a=i+1;
+
     }
 }
