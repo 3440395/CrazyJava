2f618d7962b07c07f81f9c558eb886ce559ec189
薛凯 <3440395@qq.com>
2017-04-28 12:22:48 +0800

    五一前最后一次

--- code/ThinkingInJava/app/src/main/java/com/xk/sample/initialization/MethodInit3.java
+++ code/ThinkingInJava/app/src/main/java/com/xk/sample/initialization/MethodInit3.java
@@ -1,7 +1,7 @@
 //: initialization/MethodInit3.java
 package com.xk.sample.initialization; /* Added by Eclipse.py */
 public class MethodInit3 {
-  //! int j = g(i); // Illegal forward reference
+//   int j = g(i); // Illegal forward reference
   int i = f();
   int f() { return 11; }
   int g(int n) { return n * 10; }
