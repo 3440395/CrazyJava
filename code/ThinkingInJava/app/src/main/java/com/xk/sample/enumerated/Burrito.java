a70dba3b2285531da53c69a2528ea8ba85693452
薛凯 <3440395@qq.com>
2017-05-16 18:05:16 +0800

    枚举

---
+++ code/ThinkingInJava/app/src/main/java/com/xk/sample/enumerated/Burrito.java
@@ -0,0 +1,26 @@
+//: enumerated/Burrito.java
+package com.xk.sample.enumerated;
+
+import com.xk.sample.initialization.Spiciness;
+
+public class Burrito {
+    Spiciness degree;
+
+    public Burrito(Spiciness degree) {
+        this.degree = degree;
+    }
+
+    public String toString() {
+        return "Burrito is " + degree;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Burrito(Spiciness.NOT));
+        System.out.println(new Burrito(Spiciness.MEDIUM));
+        System.out.println(new Burrito(Spiciness.HOT));
+    }
+} /* Output:
+Burrito is NOT
+Burrito is MEDIUM
+Burrito is HOT
+*///:~
